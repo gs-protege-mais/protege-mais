1a8e3536be633543194dfe44823796334d59c94f
Mari <rm566357@fiap.com.br>
2025-05-30 20:59:47 -0300

    Primeiro Commmit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// navbar
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "images/menu.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "images/fechar.png";
+    }
+}
+
+
+// faq
+
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', () => {
+        const body = accordion.querySelector('.accordion-body');
+        const arrow = accordion.querySelector('.arrow');
+
+        body.classList.toggle('active');
+        arrow.classList.toggle('rotate');
+
+    })
+})
+
+
